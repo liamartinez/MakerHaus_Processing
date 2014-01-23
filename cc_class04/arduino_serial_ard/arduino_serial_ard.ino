609e743e727d76ec6ade2fd444292b845ba781a3
liamartinez <sushionthego@gmail.com>
2014-01-22 20:01:06 -0800

    moved old

--- cc_class04/arduino_serial_ard/arduino_serial_ard.ino
+++
@@ -1,7 +0,0 @@
-void setup() {
-    Serial.begin(9600);
-}
-void loop() {
-    int analogValue = (A2) /4;      // read the pot value
-    Serial.write(analogValue);        // print the value in the serial monitor as a binary value
-}
